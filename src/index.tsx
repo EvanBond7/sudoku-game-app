d6481aeb8f524d4e40b2445042fd657242d114fb
Evan Bond <43074752+EvanBond7@users.noreply.github.com>
2022-01-19 20:31:02 +0300

    Add styled-components with a global theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 
 import { unregister } from './core';
+import { GlobalStyle, theme } from './style';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <div>START APP!</div>
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <div>Sudoku App!</div>
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
