d7ad40e2a33d93ec5aa8dd9596dec6196943f08e
Evan Bond <43074752+EvanBond7@users.noreply.github.com>
2022-01-20 14:06:44 +0300

    Add Block component

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 
+import { Content, Title, Card, Grid } from './components';
 import { unregister } from './core';
 import { GlobalStyle, theme } from './style';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyle />
-    <div>Sudoku App!</div>
+
+    <Content>
+      <Title>Sudoku App</Title>
+      <Card>
+        <Grid />
+      </Card>
+    </Content>
   </ThemeProvider>,
   document.getElementById('root')
 );
--- src/style/theme.ts
+++ src/style/theme.ts
@@ -3,7 +3,7 @@ export const theme = {
     background: 'radial-gradient(#282c34cc, #282c34)',
     black: '#282c34',
     blue: '#a0e9fd',
-    lightBLue: '#caf3fe',
+    lightBlue: '#caf3fe',
     white: 'white',
   },
 
